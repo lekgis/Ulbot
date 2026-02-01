4fb59271f0a41595b99587cbab1129285b74457b
lekgis <lekgis1@gmail.com>
2026-02-01 15:32:47 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'lbot-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/icons/lb.ico'
+];
+
+// ติดตั้งครั้งแรก - แคชไฟล์
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// ดึงข้อมูล - ใช้แคชก่อน ถ้าไม่มีค่อยโหลดจากเน็ต
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
